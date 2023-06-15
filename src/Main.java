26abf557f649f03f7e9e197e245425e53fccd253
Margarita Kosareva <kosareva-r@ya.ru>
2023-06-15 14:51:40 +0300

    Initial commits

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int tiketPrice = 150;
+        int milePrice = 20;
+        int mileCount = tiketPrice / milePrice;
+        System.out.println("Kоличество бонусных миль: " + mileCount);
+    }
+}
